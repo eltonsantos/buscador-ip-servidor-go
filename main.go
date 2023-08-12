9e54900465b24af3e05e3f3d1bae3015085305ac
Elton Santos <eltonaxl@hotmail.com>
2023-08-12 00:42:00 -0300

    First commmit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	aplication := app.Gerar()
+	if erro := aplication.Run(os.Args); erro != nil {
+		log.Fatal(erro)
+	}
+}
